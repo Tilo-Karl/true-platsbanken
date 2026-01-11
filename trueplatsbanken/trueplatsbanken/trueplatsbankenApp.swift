02618e8fa23006e8c43191d64f19baaef0754c4f
tilo_delau <tdelau@gmail.com>
2026-01-11 17:27:47 +0100

    Monorepo: backend added, Firebase init wired

--- trueplatsbanken/trueplatsbanken/trueplatsbankenApp.swift
+++ trueplatsbanken/trueplatsbanken/trueplatsbankenApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct trueplatsbankenApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
