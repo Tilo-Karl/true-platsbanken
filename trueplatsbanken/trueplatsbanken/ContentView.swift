a7b5bcffd328ccb33605674d4487aed37a23b88f
tilo_delau <tdelau@gmail.com>
2026-01-11 13:39:44 +0100

    Initial Xcode project

---
+++ trueplatsbanken/trueplatsbanken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trueplatsbanken
+//
+//  Created by Tilo Delau on 2026-01-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trueplatsbanken/trueplatsbanken/trueplatsbankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  trueplatsbankenApp.swift
+//  trueplatsbanken
+//
+//  Created by Tilo Delau on 2026-01-11.
+//
+
+import SwiftUI
+
+@main
+struct trueplatsbankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
